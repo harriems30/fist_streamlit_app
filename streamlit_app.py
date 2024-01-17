0879e6feb05e010c7d4069ab47586a234bb1e695
harriems30 <156664835+harriems30@users.noreply.github.com>
2024-01-17 12:07:58 +0000

    Update streamlit_app.py
    create a table from response data

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,14 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+# requests module to access api
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# turn json into a form that can become a table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# create table
+streamlit.dataframe(fruityvice_normalized)
 
 
