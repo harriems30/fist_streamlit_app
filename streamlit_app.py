b419844349f3c234286b49c8267d83243918af37
harriems30 <156664835+harriems30@users.noreply.github.com>
2024-01-17 12:15:03 +0000

    Update streamlit_app.py
    remove json line without table

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 # requests module to access api
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # turn json into a form that can become a table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
