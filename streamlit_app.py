a7b524a09135380285cb3f850bf3a72360a4188b
harriems30 <156664835+harriems30@users.noreply.github.com>
2024-01-22 12:00:35 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ streamlit.write('The user entered ', fruit_choice)
 # create table
 
 
-streamlit.stop()
+#streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
