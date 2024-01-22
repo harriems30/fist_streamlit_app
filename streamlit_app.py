82052a58290e0328baa8290e5bd3c8bfa8ea0166
harriems30 <156664835+harriems30@users.noreply.github.com>
2024-01-22 14:34:02 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,13 +49,10 @@ except URLError as e:
 
 streamlit.write('The user entered ', fruit_choice)
 
-
-streamlit.stop()
-
 # snowflake functions
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur
+    with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
 # add button 
